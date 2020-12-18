ee42b10ea1ffb500c3bf24894d49a966a04ccf26
MJ <44271342+MjedAl@users.noreply.github.com>
2020-12-18 06:30:03 +0300

    upload

---
+++ config.py
@@ -0,0 +1,5 @@
+currentSemesterBodyPath = '/html/body/div[5]/table[45]/tbody' # Change to the Xpath of the current semester table body
+gmail_user = '#####@gmail.com'
+gmail_password = '#####'
+odus_password = '#####'
+odus_id = '#####'
---
+++ main.py
@@ -0,0 +1,62 @@
+from secure import id, MyPassword
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
+from datetime import datetime
+from config import odus_id, gmail_user, gmail_password, odus_password, currentSemesterBodyPath
+import smtplib
+
+foundCourses = []
+sent_from = gmail_user
+to = ['#####'] # Change to the recipient email
+body = 'Please check OdusPlus'
+path = "\chromedriver.exe"
+driver = webdriver.Chrome()# Open the website
+driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+
+def login():
+    username = driver.find_element_by_id('UserID')
+    password = driver.find_element_by_xpath('//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/input[1]')
+    username.send_keys(odus_id)
+    password.send_keys(odus_password)
+    loginButtonPath = '//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td[4]/a/img'
+    click = driver.find_element_by_xpath(loginButtonPath).click()
+    driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+
+login()
+while True:
+    driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+    html_source = driver.page_source
+    if '<img src="../wtlgifs/banner/login.jpg" border="0">' in html_source:
+        login()
+    GradesTable = driver.find_element_by_xpath(currentSemesterBodyPath)
+    rows = GradesTable.find_elements_by_xpath('./tr')
+    iterRows = iter(rows)
+    next(iterRows)
+    for row in iterRows:
+        courseCode = row.text.split()[0]+row.text.split()[1]
+        if courseCode not in foundCourses:
+            print('New grade detected for '+courseCode)
+            print('Time: '+str(datetime.now()))
+            foundCourses.append(courseCode)
+            subject = 'New grade detected on OdusPlus for '+courseCode
+            email_text = """\
+From: %s
+To: %s
+Subject: %s
+%s
+            """ % (sent_from, ", ".join(to), subject, body)
+            try:
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                server.login(gmail_user, gmail_password)
+                server.sendmail(sent_from, to, email_text)
+                server.close()
+                print ('Email sent!')
+            except:
+                print ('Something went wrong...')
+    print('Sleeping for 15m')
+    time.sleep(900)
