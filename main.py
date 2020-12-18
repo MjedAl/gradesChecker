98cbed64dd5574a2773e7a075dd6d1435fc9501e
Abdulmajeed Al-ahmadi <44271342+MjedAl@users.noreply.github.com>
2020-12-18 06:54:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ def login():
     loginButtonPath = '//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td[4]/a/img'
     click = driver.find_element_by_xpath(loginButtonPath).click()
     driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+    time.sleep(2) # Time to load
 
 login()
 while True:
     driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+    time.sleep(2) # Time to load
     html_source = driver.page_source
     if '<img src="../wtlgifs/banner/login.jpg" border="0">' in html_source or 'تم اكتشاف محاولة دخول غير مسموح بها! الرجاء تسجيل الدخول مرة أخرى.' in html_source:
         login()
