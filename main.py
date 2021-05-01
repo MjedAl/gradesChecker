831c782bdfa302537edbdd2d5da5e5593cb042a0
Abdulmajeed Al-ahmadi <44271342+MjedAl@users.noreply.github.com>
2021-05-01 03:00:32 +0300

    Bug fix
    Prevent sending emails on the first run

--- main.py
+++ main.py
@@ -10,26 +10,31 @@ import smtplib
 
 foundCourses = []
 sent_from = gmail_user
-to = ['#####'] # Change to the recipient email
+to = [gmail_user]
 body = 'Please check OdusPlus'
 path = "\chromedriver.exe"
-driver = webdriver.Chrome()# Open the website
+driver = webdriver.Chrome()  # Open the website
 driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+
+firstRun = True
+
 
 def login():
     username = driver.find_element_by_id('UserID')
-    password = driver.find_element_by_xpath('//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/input[1]')
+    password = driver.find_element_by_xpath(
+        '//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/input[1]')
     username.send_keys(odus_id)
     password.send_keys(odus_password)
     loginButtonPath = '//*[@id="loginform"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td[4]/a/img'
     click = driver.find_element_by_xpath(loginButtonPath).click()
     driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
-    time.sleep(2) # Time to load
+    time.sleep(2)  # Time to load
+
 
 login()
 while True:
     driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
-    time.sleep(2) # Time to load
+    time.sleep(2)  # Time to load
     html_source = driver.page_source
     if '<img src="../wtlgifs/banner/login.jpg" border="0">' in html_source or 'تم اكتشاف محاولة دخول غير مسموح بها! الرجاء تسجيل الدخول مرة أخرى.' in html_source:
         login()
@@ -40,7 +45,7 @@ while True:
     for row in iterRows:
         courseCode = row.text.split()[0]+row.text.split()[1]
         grade = row.find_elements_by_xpath('./td')
-        if grade[5].text != ' ': # grade is not posted yet
+        if grade[5].text != ' ':  # grade is not posted yet
             if courseCode not in foundCourses:
                 print('New grade detected for '+courseCode)
                 print('Time: '+str(datetime.now()))
@@ -52,14 +57,16 @@ To: %s
 Subject: %s
 %s
                 """ % (sent_from, ", ".join(to), subject, body)
-                try:
-                    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-                    server.ehlo()
-                    server.login(gmail_user, gmail_password)
-                    server.sendmail(sent_from, to, email_text)
-                    server.close()
-                    print ('Email sent!')
-                except:
-                    print ('Something went wrong...')
+                if not firstRun:
+                    try:
+                        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                        server.ehlo()
+                        server.login(gmail_user, gmail_password)
+                        server.sendmail(sent_from, to, email_text)
+                        server.close()
+                        print('Email sent!')
+                    except:
+                        print('Something went wrong...')
+    firstRun = False
     print('Sleeping for 15m')
     time.sleep(900)
