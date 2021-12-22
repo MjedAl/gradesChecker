2e08969968a94091d81259ffd34373cbe72b7c8b
Abdulmajeed Al-ahmadi <44271342+MjedAl@users.noreply.github.com>
2021-12-22 19:18:53 +0300

    Fix login issue, run as Headless

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from datetime import datetime
-from config import odus_id, gmail_user, gmail_password, odus_password, currentSemesterBodyPath
+from config import gmail_user, gmail_password, odus_id, odus_password, currentSemesterBodyPath
 import smtplib
 
 foundCourses = []
@@ -13,19 +11,25 @@ sent_from = gmail_user
 to = [gmail_user]
 body = 'Please check OdusPlus'
 path = "\chromedriver.exe"
-driver = webdriver.Chrome()  # Open the website
-driver.get('https://odusplus-ss.kau.edu.sa/')
-
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=800,900")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 firstRun = True
 
 
 def login():
-    username = driver.find_element_by_id('userid')
-    password = driver.find_element_by_id('password')
+    driver.get('https://odusplus-ss.kau.edu.sa/')
+    username = driver.find_element(By.ID, 'userid')
+    password = driver.find_element(By.ID, 'password')
     username.send_keys(odus_id)
     password.send_keys(odus_password)
     loginButtonPath = '/html/body/div/div[2]/div/div[1]/div/form/button'
-    driver.find_element_by_xpath(loginButtonPath).click()
+    driver.find_element(By.XPATH, loginButtonPath).click()
     time.sleep(2)  # Time to load
 
 
@@ -36,26 +40,28 @@ while True:
     html_source = driver.page_source
     if '<img src="../wtlgifs/banner/login.jpg" border="0">' in html_source or 'تم اكتشاف محاولة دخول غير مسموح بها! الرجاء تسجيل الدخول مرة أخرى.' in html_source or 'تسجيل دخول جامعة الملك عبدالعزيز' in html_source:
         login()
-    GradesTable = driver.find_element_by_xpath(currentSemesterBodyPath)
-    rows = GradesTable.find_elements_by_xpath('./tr')
+        driver.get('https://odusplus-ss.kau.edu.sa/PROD/ywsksinf.P_Display_All_Info')
+        time.sleep(2)  # Time to load
+    GradesTable = driver.find_element(By.XPATH,currentSemesterBodyPath)
+    rows = GradesTable.find_elements(By.XPATH,'./tr')
     iterRows = iter(rows)
     next(iterRows)
     for row in iterRows:
         courseCode = row.text.split()[0]+row.text.split()[1]
-        grade = row.find_elements_by_xpath('./td')
+        grade = row.find_elements(By.XPATH,'./td')
         if grade[5].text != ' ':  # grade is not posted yet
             if courseCode not in foundCourses:
                 print('New grade detected for '+courseCode)
                 print('Time: '+str(datetime.now()))
-                foundCourses.append(courseCode)
-                subject = 'New grade detected on OdusPlus for '+courseCode
-                email_text = """\
+                foundCourses.append(courseCode)                    
+                if not firstRun:
+                    subject = 'New grade detected on OdusPlus for '+courseCode
+                    email_text = """\
 From: %s
 To: %s
 Subject: %s
 %s
-                """ % (sent_from, ", ".join(to), subject, body)
-                if not firstRun:
+                    """ % (sent_from, ", ".join(to), subject, body)
                     try:
                         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
                         server.ehlo()
